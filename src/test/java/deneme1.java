653f3a78074e4e8d62fbb0a7198e0a1122e1c8bd
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2021-10-14 23:38:46 +0100

    first

---
+++ src/test/java/deneme1.java
@@ -0,0 +1,11 @@
+public class deneme1 {
+    /*
+        echo "# Jenkins" >> README.md
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/omeryttnc/Jenkins.git
+        git push -u origin main
+     */
+}
