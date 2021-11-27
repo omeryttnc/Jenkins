da94be1e184b3bd3a24fe630c681138952d4a5e4
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2021-11-27 01:49:19 +0000

    g

---
+++ src/test/java/l.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class l {
+    @Test
+    public void print() {
+        System.out.println("Hello World");
+        System.out.println("sss");
+    }
+
+    @Test
+    public void testName() {
+        System.out.println("a");
+    }
+}
