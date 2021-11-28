d447d04ee2b4ab7b49ddfa469354be4b16ecf2e1
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2021-11-28 18:02:43 +0000

    g

--- src/test/java/deneme1.java
+++ src/test/java/deneme1.java
@@ -1,13 +1,13 @@
 import org.testng.annotations.Test;
 
 public class deneme1 {
-    @Test
+    @Test(groups = "smoke")
     public void print() {
         System.out.println("Hello World");
         System.out.println("sss");
     }
 
-    @Test
+    @Test(groups = "smoke")
     public void testName() {
         System.out.println("a");
     }
