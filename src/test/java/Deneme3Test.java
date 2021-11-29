c5fd4bdb29cb8f8a705ab47fe090732f2178f710
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2021-11-29 00:12:05 +0000

    g

--- src/test/java/Deneme3Test.java
+++ src/test/java/Deneme3Test.java
@@ -1,8 +1,10 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Deneme3Test {
     @Test(groups = "regression")
     public void name(){
         System.out.println("regression testi");
+        Assert.fail();
     }
 }
