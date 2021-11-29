29c6adb66654f69cef6270b546ba4278a2ff72be
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2021-11-29 11:31:45 +0000

    g

--- src/test/java/Deneme3Test.java
+++ src/test/java/Deneme3Test.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.Driver;
 
 public class Deneme3Test {
     @Test(groups = "regression")
@@ -7,4 +10,28 @@ public class Deneme3Test {
         System.out.println("regression testi");
         System.out.println("s");
     }
+
+    @Test(groups = "regression")
+    public void testGoogleSearch() throws InterruptedException {
+
+        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+         System.setProperty("webdriver.chrome.driver", "C:/Users/savsa/Desktop/chromedriver_win32/chromedriver.exe");
+         WebDriver driver = new ChromeDriver();
+
+
+        Thread.sleep(5000);  // Let the user actually see something!
+
+
+        driver.get("https://stackoverflow.com/");
+
+        Thread.sleep(5000);  // Let the user actually see something!
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "https://stackoverflow.com/");
+
+        Thread.sleep(5000);  // Let the user actually see something!
+
+        driver.quit();
+
+    }
+
 }
