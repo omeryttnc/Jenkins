e333e465cdda32a110307504a27678dae00f4ec3
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-02-19 22:52:02 +0000

    g

--- src/test/java/TEST2.java
+++ src/test/java/TEST2.java
@@ -1,10 +1,10 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TEST1 {
+public class TEST2 {
     @Test
     public void testName() {
-        Assert.fail("test 1");
+        Assert.fail("test 2");
 
     }
 }
