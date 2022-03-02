6dd311548089e8a72a6670079e7b0ed3f618e672
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-03-02 17:29:27 +0000

    g

--- src/test/java/TEST2.java
+++ src/test/java/TEST2.java
@@ -3,15 +3,39 @@ import org.testng.annotations.Test;
 import utilities.ReadTxt;
 import utilities.WriteToTxt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class TEST2 {
     @Test
     public void testName() {
         Assert.fail("test 2");
 
     }
+    public  void updateOneTime(String type, String data) {
+        Connection connection;
+        Statement statement;
+
+        String string = "jdbc:mysql://51.15.253.0:3366/urbanicfarm";
+        String username = "urbanicfarm_mysql_user";
+        String password = "urbanicfarm_mysql_password";
+        String sql = "UPDATE `tester` SET `"+type+"` = '"+data+"' WHERE `tester`.`id` = 1;";
+
+        try {
+            connection = DriverManager.getConnection(string, username, password);
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test(groups = "smoke")
     public void test() {
+
+        updateOneTime("skip","aaa");
         WriteToTxt.saveTitle("fille","icerik");
     }
 }
